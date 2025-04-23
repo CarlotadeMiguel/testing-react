782a8bcfb9dc1930cfca520562b406c85a120b46
CarlotadeMiguel <carlota.demiguel@tajamar365.com>
2025-04-23 20:11:57 +0200

    corregir nombre del css

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './LogingForm.css';
+import './LoginForm.css';
 
 const LoginForm = () => {
   const navigate = useNavigate();
